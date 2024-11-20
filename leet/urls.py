ef8dd6d27252bbbd2d3f95397215efe9d65a18b0
Albert Ashaba Aheebwa <trippleaunit@hotmail.com>
2024-11-20 22:53:19 +0300

    Add whitenoise and dockerfile

--- leet/urls.py
+++ leet/urls.py
@@ -15,19 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = (
-    [
-        path("admin/", admin.site.urls),
-        # path("", include("apps.users.urls", namespace="users")),
-        path("", include("apps.main.urls", namespace="main")),
-        path("__reload__/", include("django_browser_reload.urls")),
-    ]
-    + debug_toolbar_urls()
-    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-)
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    # path("", include("apps.users.urls", namespace="users")),
+    path("", include("apps.main.urls", namespace="main")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    from debug_toolbar.toolbar import debug_toolbar_urls
+
+    urlpatterns += debug_toolbar_urls()
+    urlpatterns += [path("__reload__/", include("django_browser_reload.urls"))]
